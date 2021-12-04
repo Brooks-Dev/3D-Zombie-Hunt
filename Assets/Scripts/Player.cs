5d0573477bb89fddf7390e81a15ab62676eb7adf
Harold Brooks <vintner@live.com>
2021-12-03 22:36:56 -0500

    added reticle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
+    [Header("Controller settings")]
     [SerializeField]
     private float _speed = 3f;
     [SerializeField]
     private float _jumpSpeed = 4.5f;
-    private float _gravity = -9.8f;
     [SerializeField]
     private float _groundDetect =1.08f;
-
+    private bool _isGrounded;
+    private float _gravity = -9.8f;
     private Vector3 _velocity;
 
-    private bool _isGrounded;
+    [Header("Camera Settings")]
+    [SerializeField]
+    private float _mouseSpeed = 2.5f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,24 +29,44 @@ public class Player : MonoBehaviour
         {
             Debug.LogError("Character controller on player is null!");
         }
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //pan left and right
+        CameraLook();
+        Movement();
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Debug.Log(Cursor.lockState);
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Debug.Log(Cursor.lockState);
+            }
+        }
+    }
+
+    private void CameraLook()
+    {
         if (Input.GetMouseButton(1))
         {
+            //pan left and right
             var currentRotation = transform.localEulerAngles;
-            currentRotation.y += Input.GetAxis("Mouse X");
+            currentRotation.y += _mouseSpeed * Input.GetAxis("Mouse X");
             transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+
             //tilt up and down
             Vector3 currentCameraRotation = Camera.main.transform.localEulerAngles;
-            currentCameraRotation.x -= Input.GetAxis("Mouse Y");
+            currentCameraRotation.x -= _mouseSpeed * Input.GetAxis("Mouse Y");
+            currentCameraRotation.x = Mathf.Clamp(currentCameraRotation.x, 0.0f, 8.0f);
             Camera.main.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right);
         }
-
-        Movement();
     }
 
     private void Movement()
@@ -67,27 +91,4 @@ public class Player : MonoBehaviour
         }
         _controller.Move(_velocity * Time.deltaTime);
     }
-
-    /*private void Movement()
-    {
-        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _groundDetect);
-        if (_isGrounded == true)
-        {
-            _velocity = Vector3.zero;
-            var horzontalIn = Input.GetAxis("Horizontal");
-            var verticalIn = Input.GetAxis("Vertical");
-            _velocity.z += _speed * verticalIn;
-            _velocity.x += _speed * horzontalIn;
-        }
-        //jump if grounded
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
-        {
-            _velocity.y = _jumpSpeed;
-        }
-        else
-        {
-            _velocity.y += _gravity * Time.deltaTime;
-        }
-        _controller.Move(_velocity * Time.deltaTime);
-    }*/
 }
